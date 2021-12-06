06d29f920136546edc671d89a60e7c6c4cf7467b
michael <mparkcode@gmail.com>
2021-12-07 08:18:50 +0900

    auth stuff

--- profiles/models.py
+++ profiles/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.db.models.base import Model
 # Create your models here.
 
+
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -24,5 +25,6 @@ class Profile(models.Model):
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(owner=instance)
+
 
 post_save.connect(create_profile, sender=User)
