63ff2f4f8b5fcc347478bc647d9fbe6bbeb4138f
michael <mparkcode@gmail.com>
2021-12-10 09:01:18 +0900

    posts and profiles queryset annotations

--- followers/serializers.py
+++ followers/serializers.py
@@ -9,7 +9,7 @@ class FollowerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Follower
-        fields = ['id', 'owner', 'followed_name', 'created_at']
+        fields = ['id', 'owner', 'followed', 'followed_name', 'created_at']
 
     def create(self, validated_data):
         try:
--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -7,6 +8,9 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     is_owner = serializers.SerializerMethodField()
+    like_id = serializers.SerializerMethodField()
+    comments_count = serializers.ReadOnlyField()
+    likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -27,10 +31,20 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Post
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title',
             'content', 'image', 'profile_id',
-            'profile_image', 'is_owner', 'image_filter'
+            'profile_image', 'is_owner', 'image_filter', 'like_id',
+            'comments_count', 'likes_count'
         ]
